b1841ec01acddb8a7888c56dd0a4680dbc1a3d53
521zzp <1409905100@qq.com>
2016-09-23 18:19:03 +0800

    first init

---
+++ app/js/components/QuestionList.js
@@ -0,0 +1,23 @@
+var React = require('react')
+var QuestionItem = require('./QuestionItem.js')
+
+module.exports = React.createClass({
+	render:function () {
+		var questions = this.props.questions; 
+		if (!Array.isArray(questions)) {
+			throw new Error('this.props.questions问题必须是数组');}
+		var questionComps = questions.map(function (qst) {
+			return <QuestionItem key = {qst.key} 
+					questionKey = {qst.key}
+					title = {qst.title}
+					desc = {qst.desc}
+					voteCount = {qst.voteCount}
+					onVote={this.props.onVote}/>
+		}.bind(this));
+		return(
+		<div id="questions" className="">
+          {questionComps}
+        </div>
+		)
+	}
+});
